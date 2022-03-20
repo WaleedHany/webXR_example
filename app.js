d50dd336d3da710a49c3f63cb4e01492a22e16d9
Waleed M. Hany <waleed.m.hany@gmail.com>
2022-03-20 20:27:36 +0200

    added first controller

--- app.js
+++ app.js
@@ -93,13 +93,37 @@ class App{
         this.renderer.xr.enabled = true;
         
         const button = new VRButton( this.renderer );
+        this.controllers = this.buildControllers()
     }
     
-    buildControllers(){
-        
+    buildControllers()
+    {
+        const controllerModelFactory = new controllerModelFactory()
+
+        const geometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new new THREE.Vector3(0, 0, -1)])
+        const line = new THREE.Line(geometry)
+        line.name = 'line'
+        line.scale.z = 0
+
+        const controllers = []
+
+        for(let i = 0; i <= 1; i++)
+        {
+            const controller = this.renderer.xr.getController(i)
+            controller.add(line.clone())
+            controller.userData.selectPressed = false
+            this.scene.add(controller)
+
+            controller.push(controller)
+
+            const grip = this.renderer.xr.getControllerGrip(i)
+            grip.add(controllerModelFactory.createControllerModel(grip))
+            this.scene.add(grip)
+        }
     }
     
-    handleController( controller ){
+    handleController( controller )
+    {
         
     }
     
