f828ab53456bf1340b6a6993a49ba4488ae8a29b
Waleed M. Hany <waleed.m.hany@gmail.com>
2022-03-20 12:57:21 +0200

    feat: first xr scene

--- app.js
+++ app.js
@@ -43,7 +43,15 @@ class App{
         this.setupXR();
         
         window.addEventListener('resize', this.resize.bind(this) );
-        
+
+        // adjust animation loop since we can't use our well known window.requestAnimationFrame() function.
+        // For VR projects we use setAnimationLoop
+        // Example:
+        //
+        // renderer.setAnimationLoop( function () {
+        //     renderer.render( scene, camera )    
+        // } )
+        //
         this.renderer.setAnimationLoop( this.render.bind(this) );
 	}	
     
@@ -53,13 +61,35 @@ class App{
     
     // Define meshs and scene components
     initScene(){
+         this.radius = 0.08;
+        // create a line box, rendered as line (not triangular faces)
+        this.room = new THREE.LineSegments(
+					new BoxLineGeometry( 6, 6, 6, 10, 10, 10 ),
+					new THREE.LineBasicMaterial( { color: 0x808080 } )
+				);
+        this.room.geometry.translate( 0, 3, 0 );
+        this.scene.add( this.room );
         
+        const geometry = new THREE.IcosahedronBufferGeometry( this.radius, 2 );
+
+        for ( let i = 0; i < 200; i ++ ) {
+
+            const object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+
+            object.position.x = this.random( -2, 2 );
+            object.position.y = this.random( -2, 2 );
+            object.position.z = this.random( -2, 2 );
+
+            this.room.add( object );
+        }
+
     }
     
     // Set up webXR library
     setupXR()
     {
-        
+        this.renderer.xr.enabled = true;
+        document.body.appendChild( VRButton.createButton( this.renderer ) );
     }
     
     resize(){
