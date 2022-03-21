1cd6fa3e64d95a6beee185b26c03740129b3af96
Waleed M. Hany <waleed.m.hany@gmail.com>
2022-03-22 01:12:51 +0200

    Ar session controller 2w2

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ class App{
 
         const btn = new ARButton( this.renderer, { onSessionStart, onSessionEnd, sessionInit: { optionalFeatures: [ 'dom-overlay' ], domOverlay: { root: document.body } } } ); 
         
-        const controller = this.renderer.xr.getController( 0 );
+        const controller = this.renderer.xr.getController( 1 );
         controller.addEventListener( 'connected', onConnected );
         
         this.scene.add( controller );
